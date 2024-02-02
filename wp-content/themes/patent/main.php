9dfd42ac9740e66e4b7d3b2ef422f26506870a50
lachuginTech <vladimir.lachugin.tech@gmail.com>
2024-02-02 16:09:23 +0300

    форма

--- wp-content/themes/patent/main.php
+++ wp-content/themes/patent/main.php
@@ -350,9 +350,45 @@ global $post;
 	<?php } ?>
 	<section class="patent-form">
 		<div class="container">
-			<h2 class="titles">
+			<h2 class="titles patent-form-titles">
 				заказать услугу патнетного поверенного
 			</h2>
+			<form class="" action="">
+				<div class="patent-form__inner">
+					<div class="patent-form__items">
+						<input type="text" placeholder="Имя">
+					</div>
+					<div class="patent-form__items">
+						<input type="text" placeholder="Email">
+					</div>
+					<div class="patent-form__items">
+						<input type="text" placeholder="Телефон">
+					</div>
+					<div class="patent-form__items">
+						<div class="patent-form__items-btn"><span>Услуга</span>
+							<svg width="14.707031" height="8.060547" viewBox="0 0 14.707 8.06055" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<desc>
+										Created with Pixso.
+								</desc>
+								<defs/>
+								<path id="Vector 2" d="" fill="#1E1D25" fill-opacity="1.000000" fill-rule="nonzero"/>
+								<path id="Vector 2" d="M0.353516 0.353516L7.35352 7.35352L14.3535 0.353516" stroke="#1E1D25" stroke-opacity="1.000000" stroke-width="1.000000"/>
+							</svg>
+					</div>
+						<div class="patent-form__list" style="display: none">
+							<div class="patent-form__item">1</div>
+						</div>
+					</div>
+				</div>
+                <div class="patent-form__inner-btn">
+                    <a class="btn dark__btn patent-form__btn" href="">Заказать услугу</a>
+                    <label for="">
+                        <input type="checkbox">
+                        <span>Я даю согласие на обработку
+моих персональных данных</span>
+                    </label>
+                </div>
+			</form>
 		</div>
 	</section>
 	<section class="articles">
