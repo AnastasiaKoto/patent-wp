f5a70d80103dd511d524b5bbb08249f2a1501207
Alex <astrodeep@ya.ru>
2024-02-19 17:54:36 +0400

    kejsy-showMore

--- wp-content/themes/patent/keyses.php
+++ wp-content/themes/patent/keyses.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 /* Template Name: Кейсы
 */
 
@@ -6,72 +6,109 @@ get_header();
 ?>
 <main class="main">
     <?php get_template_part('templates/blocks/titles', 'wrapper'); ?>
-    <?php 
-		$args = array(
-			'post_type' => 'keyses',
-			'post_status' => 'publish',
-			'posts_per_page' => -1,
-		);
-		$keyses = new WP_Query($args);
-		if ($keyses->have_posts()) {
-	?>
+    <?php
+    $args = array(
+        'post_type' => 'keyses',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+    );
+    $keyses = new WP_Query($args);
+    if ($keyses->have_posts()) {
+        ?>
         <section class="keyses-section">
             <div class="container">
                 <div class="tabs keyses__tabs">
                     <ul class="tabs__caption">
-                        <?php 
+                        <?php
                         $count_tab = 1;
                         while ( $keyses->have_posts() ) {
                             $keyses->the_post();
-                        ?>
-                        <li <?php if($count_tab == 1) { ?> class="active" <?php } ?>><?php the_title(); ?></li>
-                        <?php $count_tab++;  } ?>
+                            ?>
+                            <li <?php if($count_tab == 1) { ?> class="active" <?php } ?>><?php the_title(); ?></li>
+                            <?php $count_tab++;  } ?>
                     </ul>
-                    <?php 
+                    <?php
                     $count_keys = 1;
                     while ( $keyses->have_posts() ) {
                         $keyses->the_post();
-                    ?>
-                    <div class="tabs__content <?php if($count_keys == 1) { ?> active <?php } ?>">
-                        <?php 
-                        $keyses_field = get_field('servise_keyses', get_the_ID());
-                        if($keyses_field) {
                         ?>
-                        <div class="keyses__wrapper">
-                            <?php foreach($keyses_field as $keys) { ?>
-                            <div class="keyses-slide flex">
-                                <div class="keyses__left">
-                                    <img src="<?php echo $keys['logo']; ?>" alt="">
-                                    <div class="keys__descr">
-                                        <?php echo $keys['short_descr']; ?>
-                                    </div>
+                        <div class="tabs__content <?php if($count_keys == 1) { ?> active <?php } ?>">
+                            <?php
+                            $keyses_field = get_field('servise_keyses', get_the_ID());
+                            if($keyses_field) {
+                                $num_keys = count($keyses_field);
+                                $counter = 0;
+                                ?>
+                                <div class="keyses__wrapper">
+                                    <?php
+                                    foreach($keyses_field as $keys) {
+                                        $counter++;
+                                        ?>
+                                        <style>
+                                            .keyses-slide.flex.hidden{
+                                                display: none !important;
+                                            }
+                                        </style>
+                                        <div class="keyses-slide flex <?php if ($counter > 4) echo 'hidden'; ?>">
+                                            <div class="keyses__left">
+                                                <img src="<?php echo $keys['logo']; ?>" alt="">
+                                                <div class="keys__descr">
+                                                    <?php echo $keys['short_descr']; ?>
+                                                </div>
+                                            </div>
+                                            <div class="keyses__right">
+                                                <div class="title">
+                                                    Задача
+                                                </div>
+                                                <div class="descr">
+                                                    <?php echo $keys['task']; ?>
+                                                </div>
+                                                <div class="title">
+                                                    что сделали
+                                                </div>
+                                                <div class="descr">
+                                                    <?php echo $keys['result']; ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php
+                                    }
+                                    ?>
                                 </div>
-                                <div class="keyses__right">
-                                    <div class="title">
-                                        Задача
-                                    </div>
-                                    <div class="descr">
-                                        <?php echo $keys['task']; ?>
-                                    </div>
-                                    <div class="title">
-                                        что сделали
-                                    </div>
-                                    <div class="descr">
-                                        <?php echo $keys['result']; ?>
-                                    </div>
-                                </div>
-                            </div>
+                            <?php if ($num_keys > 4) : ?>
+                                <a class="btn dark__btn" id="showMoreKeyses">Показать еще</a>
+                                <script>
+                                    document.addEventListener('DOMContentLoaded', function() {
+                                        var showMoreButton = document.getElementById('showMoreKeyses');
+                                        if (showMoreButton) {
+                                            showMoreButton.addEventListener('click', function() {
+                                                var hiddenSlides = document.querySelectorAll('.keyses-slide.hidden');
+                                                var counter = 0;
+                                                hiddenSlides.forEach(function(slide) {
+                                                    if (counter < 4) {
+                                                        slide.classList.remove('hidden');
+                                                        counter++;
+                                                    }
+                                                });
+                                                if (document.querySelectorAll('.keyses-slide.hidden').length === 0) {
+                                                    showMoreButton.style.display = 'none';
+                                                }
+                                            });
+                                        }
+                                    });
+                                </script>
+
+                            <?php endif; ?>
+
                             <?php } ?>
                         </div>
-                        <?php } ?>
-                    </div>
-                    <?php $count_keys++; } ?>
+                        <?php $count_keys++; } ?>
                 </div>
             </div>
         </section>
-	<?php } else {
-	wp_reset_postdata(); 
-	?>
+    <?php } else {
+        wp_reset_postdata();
+        ?>
         <div class="no-found">Извините, ничего не найдено</div>
     <?php } ?>
 </main>
