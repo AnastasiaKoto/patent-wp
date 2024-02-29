860e8f343a11a25104444a598ab1ec761c05eff1
kryukovD <98350191+kryukovD@users.noreply.github.com>
2024-02-29 13:04:05 +0300

    created 404 page

--- wp-content/themes/patent/404.php
+++ wp-content/themes/patent/404.php
@@ -12,22 +12,53 @@
 get_header();
 ?>
 
-	<div id="primary" class="test content-area">
-		<main id="main" class="site-main">
 
-			<div class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
-				</header><!-- .page-header -->
+<section class="section-404">
+	<div class="container">
+		<div class="section-404__inner">
+			<div class="section-404__header section-404-header">
+				<div class="section-404-header__title"> Патентный поверенный № 1387 </div>
+				<div class="section-404-header__subtitle">Юрист Анатолий Павлов </div>
+			</div>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</div><!-- .error-404 -->
+			<div class="section-404__body section-404-body">
+				<div class="section-404-body__text">
+					К сожалению, такой страницы не существует
+				</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				<div class="section-404-body__image">
+					<img src="/wp-content/themes/patent/images/404-img.png" alt="404">
+				</div>
+
+			</div>
+			<div class="section-404-body__bottom">
+				<a class="btn-404" href="/">Вернуться на главную </a>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+
+
+<?/*
+<div id="primary" class="test content-area">
+  <main id="main" class="site-main">
+
+	  <div class="error-404 not-found">
+		  <header class="page-header">
+			  <h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
+		  </header><!-- .page-header -->
+
+		  <div class="page-content">
+			  <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
+			  <?php get_search_form(); ?>
+		  </div><!-- .page-content -->
+	  </div><!-- .error-404 -->
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+*/ ?>
 
 <?php
 get_footer();
