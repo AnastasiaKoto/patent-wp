52ca83829f9beacbabb47bc53c77adb384fe7280
Alex <astrodeep@ya.ru>
2024-03-01 14:36:46 +0400

    header-mobile

--- wp-content/themes/patent/header.php
+++ wp-content/themes/patent/header.php
@@ -80,49 +80,68 @@
     <header class="header-mobile">
         <div class="container header-mobile__container">
             <div class="header-mobile__top">
-                <p class="header-mobile__info">
-                    <span class="header-mobile__text--400">Патентный поверенный № 1387</span>
-                    <span class="header-mobile__text--600">Юрист Анатолий Павлов</span>
-                </p>
+                <a href="<?php echo home_url(); ?>" class="header__logo">
+                    <img src="<?php echo get_field('logo', 'option'); ?>">
+                </a>
                 <nav class="nav" data-nav>
                     <ul class="nav__list">
-                        <li class="nav__item">
-                            <a href="#" class="header-mobile__link">Главная</a>
-                        </li>
-                        <li class="nav__item">
-                            <a href="#" class="header-mobile__link">Услуги</a>
-                        </li>
-                        <li class="nav__item">
-                            <a href="#" class="header-mobile__link">О патентном бюро</a>
-                        </li>
-                        <li class="nav__item">
-                            <a href="#" class="header-mobile__link">МКТУ</a>
-                        </li>
-                        <li class="nav__item">
-                            <a href="#" class="header-mobile__link">Кейсы</a>
-                        </li>
+                        <?php
+                        function add_additional_class_on_a($classes, $item, $args)
+                        {
+                            if (isset($args->add_a_class)) {
+                                $classes['class'] = $args->add_a_class;
+                            }
+                            return $classes;
+                        }
+                        add_filter('nav_menu_link_attributes', 'add_additional_class_on_a', 1, 3);
+
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'header',
+                                'container'      => false,
+                                'items_wrap'     => '%3$s',
+                                'add_a_class'     => 'header-mobile__link',
+
+                            )
+                        );
+                        ?>
                         <li class="nav__item">
                             <ul class="header-mobile__social header-mobile__social-2">
-                                <li>
-                                    <a href="#">
-                                        <img src="/wp-content/themes/patent/images/Telegram.svg" alt="" width="44" />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <img src="/wp-content/themes/patent/images/WhatsApp.svg" alt="" width="44" />
-                                    </a>
-                                </li>
+                                <?php if (get_field('tg_link', 'option')) { ?>
+                                    <li>   <a href="<?php echo get_field('tg_link', 'option'); ?>">
+                                            <img src="/wp-content/themes/patent/images/Telegram.svg" alt="" width="44">
+                                        </a>   </li>
+                                <?php } ?>
+
+                                <?php if (get_field('wa_link', 'option')) { ?>
+                                    <li> <a href="<?php echo get_field('wa_link', 'option'); ?>">
+                                        <img src="/wp-content/themes/patent/images/WhatsApp.svg" alt="" width="44">
+                                    </a>     </li>
+                                <?php } ?>
+
                             </ul>
                         </li>
                         <li class="nav__item nav__item-2">
-                            <a href="#" class="header-mobile__link-2">info@gospatent.ru</a>
+                            <a class="header-mobile__link-2" href="mailto:<?php echo get_field('header_mail', 'option'); ?>">
+                                <?php echo get_field('header_mail', 'option'); ?>
+                            </a>
                         </li>
                         <li class="nav__item">
-                            <a href="#" class="header-mobile__link-2">+7 (926) 207-17-90</a>
+                            <?php
+                            if (have_rows('header_tel', 'option')) {
+                                while (have_rows('header_tel', 'option')) {
+                                    the_row();
+                                    $tel_link = get_sub_field('header-tel_link');
+                                    $tel_tel  = get_sub_field('header-tel_tel');
+                                    ?>
+                                    <a class="header-mobile__link-2" href="tel:<?php echo $tel_link; ?>">
+                                        <?php echo $tel_tel; ?>
+                                    </a>
+                                <?php }
+                            } ?>
                         </li>
                         <li class="nav__item">
-                            <a href="#" class="header-mobile__btn">Заказать звонок</a>
+                            <a  href="#callback-popup" class="  global-trigger header-mobile__btn">Заказать звонок</a>
                         </li>
                     </ul>
                 </nav>
@@ -132,23 +151,36 @@
             </div>
 
             <div class="header-mobile__bottom">
-                <a class="header-mobile__text--500" href="#">+7 (926) 207-17-90</a>
-                <a class="header-mobile__text--500" href="#">info@gospatent.ru</a>
+                <?php
+                if (have_rows('header_tel', 'option')) {
+                    while (have_rows('header_tel', 'option')) {
+                        the_row();
+                        $tel_link = get_sub_field('header-tel_link');
+                        $tel_tel  = get_sub_field('header-tel_tel');
+                        ?>
+                        <a class="header-mobile__text--500" href="tel:<?php echo $tel_link; ?>">
+                            <?php echo $tel_tel; ?>
+                        </a>
+                    <?php }
+                } ?>
+                <a class="header-mobile__text--500" href="mailto:<?php echo get_field('header_mail', 'option'); ?>">
+                    <?php echo get_field('header_mail', 'option'); ?>
+                </a>
+
                 <ul class="header-mobile__social">
-                    <li>
-                        <a href="#">
-                            <img src="/wp-content/themes/patent/images/Telegram.svg" alt="" width="31" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <img src="/wp-content/themes/patent/images/WhatsApp.svg" alt="" width="31" />
-                        </a>
-                    </li>
+                    <?php if (get_field('tg_link', 'option')) { ?>
+                        <li>   <a href="<?php echo get_field('tg_link', 'option'); ?>">
+                                <img src="/wp-content/themes/patent/images/Telegram.svg" alt="" width="31">
+                            </a> </li>
+                    <?php } ?>
+
+                    <?php if (get_field('wa_link', 'option')) { ?>
+                        <li> <a href="<?php echo get_field('wa_link', 'option'); ?>">
+                                <img src="/wp-content/themes/patent/images/WhatsApp.svg" alt="" width="31">
+                            </a> </li>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
     </header>
-
-
     <div id="content" class="site-content">
