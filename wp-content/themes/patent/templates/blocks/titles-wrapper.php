7be4e27aac7e4a68306a3da3d195855003573b6c
Alex <astrodeep@ya.ru>
2024-03-06 11:18:29 +0400

    fix services title

--- wp-content/themes/patent/templates/blocks/titles-wrapper.php
+++ wp-content/themes/patent/templates/blocks/titles-wrapper.php
@@ -1,12 +1,16 @@
 <section class="titles__block <?=is_page(381)?'titles__block_dark titles__block_office':'' ?>" >
     <div class="container">
         <div class="breadcrumbs ">
-            <?php 
+            <?php
             if ( function_exists('yoast_breadcrumb') ) {
             yoast_breadcrumb();
             }
             ?>
         </div>
-        <h1 class="titles page__title"><?php the_title(); ?></h1>
+        <?php if( is_archive() ) : ?>
+        <h1 class="titles page__title"><?php post_type_archive_title(); ?></h1>
+        <?php else : ?>
+            <h1 class="titles page__title"><?php the_title(); ?></h1>
+        <?php endif; ?>
     </div>
 </section>
